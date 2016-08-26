f95446fac5e50584d18751599adcb76899e9fb93
dimanklg <dimankaliningrad@gmail.com>
2016-08-26 19:58:18 +0100

    server edit

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,16 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
+    'webpack-dev-server/client?http://localhost:3030'
   ],
   output: {
-    path: path.join(__dirname, ''),
-    filename: 'bundle.js',
-    publicPath: 'build/'
+    path: path.join(__dirname, '')
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
     }]
   }
