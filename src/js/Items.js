088f640b8bf9979b5b31133935687cf7a0d673e1
dimanklg <dimankaliningrad@gmail.com>
2016-09-30 23:18:08 +0200

    add jasmine

--- src/js/Items.js
+++ src/js/Items.js
@@ -6,8 +6,19 @@
 				elements:[],
 				get: function(){
 					return $http.get("dataModel.json")
-
-				}
+				},
+				dataArr:[
+				{productNumber: 411,
+				 amount:2},
+				{productNumber: 132,
+				 amount:4},
+				{productNumber: 567,
+				 amount:2},
+				{productNumber: 578,
+				 amount:1},
+				{productNumber: 243,
+				 amount:8},
+			]
 			}
 
 			return Items;
